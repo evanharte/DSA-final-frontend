9ba0c3090ed07fec172014c0bfdf898c20df6f1d
Evan <evan.harte@keyin.com>
2024-08-12 17:05:32 -0230

    implemented fetch previous trees

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosInstance from "./axiosInstance";
 import "./App.css";
 import photo from "./media/santa.webp";
 
 function App() {
   const [numbers, setNumbers] = useState("");
   const [tree, setTree] = useState(null);
+  const [previousTrees, setPreviousTrees] = useState([]);
   const [error, setError] = useState("");
 
   const fetchData = () => {
@@ -13,8 +14,8 @@ function App() {
       .split(",")
       .map((num) => parseInt(num.trim(), 10));
 
-    axios
-      .post("http://localhost:8080/api/create-tree", numbersArray)
+    axiosInstance
+      .post("/create-tree", numbersArray)
       .then((response) => {
         const treeData = response.data;
         setTree(treeData);
@@ -26,11 +27,24 @@ function App() {
       });
   };
 
+  const fetchPreviousTrees = () => {
+    axiosInstance
+      .get("/previous-trees")
+      .then((response) => {
+        setPreviousTrees(response.data);
+        setError("");
+      })
+      .catch((error) => {
+        setError("There was an error fetching the previous trees.");
+        console.error("There was an error!", error);
+      });
+  };
+
   return (
     <div className="App">
       <div className="nav">
         <div>
-          <img src={photo} />
+          <img src={photo} alt="Tree Photo" />
         </div>
         <div>
           <h1>DSA Final Project</h1>
@@ -48,12 +62,31 @@ function App() {
           </div>
         </div>
         <div>
-          <img src={photo} />
+          <img src={photo} alt="Tree Photo" />
         </div>
       </div>
       <pre className="body">
         {tree ? JSON.stringify(tree, null, 4) : "No data to display"}
       </pre>
+      <div className="previous-trees">
+        <button onClick={fetchPreviousTrees}>Fetch Previous Trees</button>
+        <div>
+          <h3>Previous Trees:</h3>
+          {previousTrees.length > 0 ? (
+            previousTrees.map((tree, index) => (
+              <div key={index}>
+                <h4>Tree {index + 1}</h4>
+                <p>Input Numbers: {tree.inputNumbers.join(", ")}</p>
+                <pre>
+                  {JSON.stringify(JSON.parse(tree.treeStructure), null, 4)}
+                </pre>
+              </div>
+            ))
+          ) : (
+            <p>No previous trees to display</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
